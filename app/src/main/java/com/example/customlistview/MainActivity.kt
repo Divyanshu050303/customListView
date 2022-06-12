839e6851732755bb527b7a31582832ad710b27d5
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-06-12 22:17:01 +0530

    main activity

--- app/src/main/java/com/example/customlistview/MainActivity.kt
+++ app/src/main/java/com/example/customlistview/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.customlistview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cityData= arrayOf(city("India", "New Delhi"),
+            city("USA", "New York"),
+            city("France", "Paris"),
+            city("Italy", "Rome"),
+            city("Netherlands", "Amsterdam"))
+        val cities=findViewById<ListView>(R.id.cities)
+        val cityAdapter=CityAdapter(cityData)
+        cities.adapter=cityAdapter
     }
 }
